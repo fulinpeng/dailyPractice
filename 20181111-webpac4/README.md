d9cf7d012e847b57e85717d12997d3c4d184b460
fulinpengwx <946205162@qq.com>
2018-11-13 22:56:17 +0800

    webpack4

--- 20181111-webpac4/README.md
+++ 20181111-webpac4/README.md
@@ -11,9 +11,12 @@
 > 注意：引入一个包的时候尽量用`{isArray, cloneDeep}`结构的形式，别一来就把整个包给引进来
 
 > 注意：一般`css-tree-shaking`用在MPA上，SPA不用
+>> 插件:purify-CSS-plugin
 
 > 代码中注释了也不行，他还是会编译，因为webpack内部是按照正则匹配的
 
 > `webpack.config.js`里面导出配置对象`必须以函数的方式`
 
 > 开始都没问题，突然出现找不到什么原因的错误，可能是`pakage-lock.js`的问题，把它删了，重新来；如：`ERROR in ./src/css/index.less (./node_modules/css-loader??ref--6-1!./node_modules/les`
+
+> css-loader依赖于style-loader；css-loader用于处理css，style-loader用来将这个js形式的style插入到html文档里面；但是这个style-loader很大，会让整个js文件剧增；
--- 20181111-webpac4/webpack.config.js
+++ 20181111-webpac4/webpack.config.js
@@ -4,15 +4,18 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const argv = require("yargs-parser")(process.argv.slice(2));
 const _mode = argv.mode || "development";
 const _modeflag = (_mode == "production" ? true : false);
+// const _mergeConfig = require(`./config/webpack.${_mode}.js`);
+const CleanWebpackPlugin = require('clean-webpack-plugin')
+
 const config = {
     entry: {
         'index': ['./src/index.js'], // 名称也可以换成路径
     },
-    // output: {
-    //     path: path.resolve(__dirname, 'dist'),
-    //     filename: '[name].[hash:5].js',
-    //     publicPath: '',
-    // },
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: '[name].[hash:5].js',
+        publicPath: '/',
+    },
     module: {
         rules: [
             {
@@ -29,6 +32,7 @@ const config = {
                     { loader: 'less-loader' }
                 ]
             },
+            // css-loader的写法之一
             {
                 test: /\.css$/,
                 use: [
@@ -38,20 +42,30 @@ const config = {
                             publicPath: './'
                         }
                     },
-                    // 'style-loader',  // 这里不需要的呀，别打开
+                    // 这里不需要style-loader的呀，别打开
                     {loader: 'css-loader'}
                 ]
             },
+            // 下面这个和上面的会冲突
             // {
-            //     test:/\.(png|jpg|gif)$/,
-            //     use:[{
-            //         loader:'url-loader',
-            //         options:{ // 这里的options选项参数可以定义多大的图片转换为base64
-            //             limit:50000, // 表示小于50kb的图片转为base64,大于50kb的是路径
-            //             outputPath:'images' //定义输出的图片文件夹
+            //     test: /\.css$/,
+            //     use: [
+            //         'style-loader',
+            //         {
+            //             loader: 'css-loader?modules&localIdentName=[name]_[local]-[hash:base64:5]'
             //         }
-            //     }]
-            // }
+            //     ]
+            // },
+            {
+                test:/\.(png|jpg|gif)$/,
+                use:[{
+                    loader:'url-loader',
+                    options:{ // 这里的options选项参数可以定义多大的图片转换为base64
+                        limit:50000, // 表示小于50kb的图片转为base64,大于50kb的是路径
+                        outputPath:'images' //定义输出的图片文件夹
+                    }
+                }]
+            }
         ]
     },
     plugins: [
@@ -63,6 +77,7 @@ const config = {
             filename:"index.html",
             template:"./src/index.html"
         }),
+        new CleanWebpackPlugin(['dist']), // npm run dist 时会自己清除dist目录
     ],
 };
 
