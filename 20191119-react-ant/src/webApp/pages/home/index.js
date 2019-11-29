eb3ad17df40e88bcf75eac0aa6bc12467f2899f2
fulinpeng <flp511@dingtalk.com>
2019-11-29 18:02:36 +0800

    react

--- 20191119-react-ant/src/webApp/pages/home/index.js
+++ 20191119-react-ant/src/webApp/pages/home/index.js
@@ -3,6 +3,7 @@ import React, { Component } from "react";
 import "./index.scss";
 
 import { Button } from 'antd';
+import Test1 from "_root/webApp/components/test1";
 
 class Home extends Component {
   constructor(props) {
@@ -10,13 +11,30 @@ class Home extends Component {
   }
 
   state = {
+    name: 'flp',
+  }
+
+  componentWillMount() {
+    setTimeout(() => {
+      this.setState({
+        name: 'gagaga'
+      })
+    }, 1000)
+  }
+
+  changeAge() {
+    console.log('click')
+    this.setState({
+      age: 22
+    })
   }
 
   render() {
     return (
       <div className="home">
-        <h1>hello!</h1>
-        <Button type="primary">Button</Button>
+        <h1>hello! {this.state.name}</h1>
+        <Button type="primary" onClick={this.changeAge.bind(this)}>Button</Button>
+        <Test1 age={this.state.age}/>
       </div>
     );
   }
